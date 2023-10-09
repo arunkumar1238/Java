3122ff058246b8411661c6a4fe235f562c5655ba
Arun Kumar A.R <allamrajarunkumar@gmail.com>
2023-10-09 22:41:46 +0530

    java

--- Sorting.java
+++ Sorting.java
@@ -10,12 +10,12 @@ class Sorting {
 
     public static void main(String args[]) {
         int arr[] = { 7, 8, 3, 1, 2 };
-        // time complexity = O(n^2)
-        // bubble sort
+        // time complexity = O(n^2);
+        // bubble sort;
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    // swap
+                    // swap;
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
